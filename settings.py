a694f32d0d477f71a4a4fe51f7627f7377795cbc
Florek <pawel.florczak87@gmail.com>
2021-01-27 21:31:47 +0100

    27.01.2021

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings:
+    def __init__(self):
+        self.alien_speed = 0.5
+        self.bg_color = (230, 230, 230)
+        self.bullet_allowed = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullet_height = 15
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 5
+        self.screen_height = 800
+        self.screen_width = 1200
+        self.ship_limit = 3
+        self.ship_speed = 1.5
